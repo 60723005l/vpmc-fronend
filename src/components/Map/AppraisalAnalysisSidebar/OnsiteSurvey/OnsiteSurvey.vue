a028db21aea17c49a7225010d09b6d060b2bf5d6
jimmg35 <jim60308@gmail.com>
2022-01-23 22:48:46 +0800

    good

--- src/components/Map/AppraisalAnalysisSidebar/OnsiteSurvey/OnsiteSurvey.vue
+++ src/components/Map/AppraisalAnalysisSidebar/OnsiteSurvey/OnsiteSurvey.vue
@@ -1,16 +1,18 @@
 <template>
     <md-tabs class="custom-tabs">
         <md-tab v-for="(tab, index) in tabs" :key="index" :id="tab.name" :md-label="tab.name">
-            <iframe :src="tab.frameSrc"></iframe>
+            <div class="frame-wrapper">
+                <iframe width="100%" height="100%" :src="tab.frameSrc"></iframe>
+            </div>
         </md-tab>
     </md-tabs>
 </template>
 <script>
 
 const tabs = [
-    {name:'土地', frameSrc:'http://140.122.82.98/Land/Create'},
-    {name:'建物', frameSrc:'http://140.122.82.98/Building/Create'},
-    {name:'車位', frameSrc:'http://140.122.82.98/Park/Create'},
+    {name:'土地', frameSrc:'https://localhost:5001/Land/Create'},
+    {name:'建物', frameSrc:'https://localhost:5001/Building/Create'},
+    {name:'車位', frameSrc:'https://localhost:5001/Park/Create'},
 ]
 export default {
     name: 'OnsiteSurvey',
@@ -23,3 +25,20 @@ export default {
         },
 }
 </script>
+<style lang="scss" scoped>
+.frame-wrapper{
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: calc(100vh - 300px);
+    iframe{
+        // position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
