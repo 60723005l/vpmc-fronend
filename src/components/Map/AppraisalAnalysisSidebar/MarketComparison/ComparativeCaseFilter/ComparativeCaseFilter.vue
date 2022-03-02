6567493a507e954cebcbfd2544331358163a6862
andy.chen <andy10030015@gmail.com>
2022-03-02 19:35:41 +0800

    refactor: update card text

--- src/components/Map/AppraisalAnalysisSidebar/MarketComparison/ComparativeCaseFilter/ComparativeCaseFilter.vue
+++ src/components/Map/AppraisalAnalysisSidebar/MarketComparison/ComparativeCaseFilter/ComparativeCaseFilter.vue
@@ -2,18 +2,30 @@
     <div>
         <StepOne @submit="handleLatlngQuery"></StepOne>
         <StepTwo @submit="handleSerialsQuery"></StepTwo>
+        <div class="filterResults">
+            <FilterResultCard
+                v-for="(item, index) in filterResults" :key="index"
+                :name="item.manageName"
+                :type="item.buildingState"
+                :unitNumber="item.unitNumber"
+                :year="item.completionDate"
+                :totalPrice="item.totalPrice"
+                :unitPrice="item.unitPrice"/>
+        </div>
     </div>
 </template>
 <script>
-import { Circle, Icon, LatLng } from 'leaflet'
+import { Circle, Icon, latLng, LatLng, Marker } from 'leaflet'
 import api from '../../../../../api'
 import Global from '../../.././../../global'
+import projector, { EPSG } from '../../../../../VPMC/module/projector'
 export default {
     name: 'ComparativeCaseFilter',
     components:
         {
             StepOne: async () => await import('./StepOne.vue'),
-            StepTwo: async () => await import('./StepTwo.vue')
+            StepTwo: async () => await import('./StepTwo.vue'),
+            FilterResultCard: async () => await import('./FilterResultCard.vue')
         },
     data() {
         return {
@@ -22,35 +34,56 @@ export default {
             mappedLayers: [],
             serials: [],
             asyncSerials: undefined,
-            params: {}
+            params: {},
+            filterResults: []
         }
     },
     mounted () {
-        Global.VPMC.transactionDataStreaming.updateEvent.addEventListener(this.handleStramingUpdate)
+        // Global.VPMC.transactionDataStreaming.updateEvent.addEventListener(this.handleStramingUpdate)
     },
     beforeDestroy () {
-        Global.VPMC.transactionDataStreaming.updateEvent.removeEventListener(this.handleStramingUpdate)
+        // Global.VPMC.transactionDataStreaming.updateEvent.removeEventListener(this.handleStramingUpdate)
     },
     methods: {
         async handleSerialsQuery (params) {
             this.params = { ...params }
             this.createBufferGraphic(this.targetedLatlng, params.distance)
             this.asyncSerials = api.Estimate.getComparativeCases(params)
-            const resp = await this.asyncSerials
-            this.serials = resp.transactionSerialNumber
-            this.hightlightMatchSerialsPoints(this.serials)
+            this.serials = await this.asyncSerials
+            console.log(this.serials)
+            const layers = this.hightlightMatchSerialsPoints(this.serials)
+            this.filterResults = layers.map(layer => layer.properties || {})
+            console.log(this.filterResults)
         },
         hightlightMatchSerialsPoints (serials = []) {
-            const points = Global.VPMC.transactionDataStreaming.pointCluster.getLayers()
-            this.mappedLayers = serials.map(id => points.find(e => e.properties.id === id))
-            .filter(layer => layer !== undefined && layer.getLatLng().distanceTo(this.targetedLatlng) <= this.params.distance)
+            const viewer = Global.VPMC.viewer
+            this.mappedLayers.forEach(layer => viewer.removeLayer(layer))
+            serials.forEach(serial => {
+                const [x, y] = projector(EPSG[3826], EPSG[4326], { x: serial.coordinateY, y: serial.coordinateX })
+                const marker = new Marker(latLng(y, x))
+                marker.properties = {...serial}
+                const content = Object.keys(serial).map(key => (`<div>${key}: ${serial[key]}</div>`))
+                marker.bindPopup(`
+                <div>
+                    ${content.join("")}
+                </div>
+                `)
+                this.mappedLayers.push(marker)
+                viewer.addLayer(marker)
+            })
+            return this.mappedLayers
+            // const points = Global.VPMC.transactionDataStreaming.pointCluster.getLayers()
+            // this.mappedLayers = serials.map(serial => points.find(e => e.properties.id === serial.id))
+            // .filter(layer => layer !== undefined && layer.getLatLng().distanceTo(this.targetedLatlng) <= this.params.distance)
 
-            this.mappedLayers.forEach(layer => layer.setStyle({
-                color: 'red'
-            }))
+            // this.mappedLayers.forEach(layer => layer.setStyle({
+            //     color: 'red'
+            // }))
+            // return this.mappedLayers
         },
         async handleStramingUpdate (points) {
-            this.hightlightMatchSerialsPoints(this.serials || [])
+            const layers = this.hightlightMatchSerialsPoints(this.serials || [])
+            this.filterResults = layers.map(layer => layer.properties || {})
         },
         /**
          * @param {LatLng} latlng
---
+++ src/components/Map/AppraisalAnalysisSidebar/MarketComparison/ComparativeCaseFilter/FilterResultCard.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="FilterResultCard">
+    <div class="title">{{name.replace('管理委員會', '')}}</div>
+    <div class="subtitle">
+      <span class="subitem">{{type}}</span>
+      <span class="subitem">{{new Date().getFullYear() - new Date(year).getFullYear()}}年</span>
+      <span class="subitem">{{unitNumber}}</span>
+    </div>
+    <div class="body">
+      <span class="bodyitem">
+        {{totalPrice/10000}}萬
+      </span>
+      <span class="bodyitem">
+        {{Math.floor(unitPrice/10000 * 3.3)}}萬/坪
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+new Date().getFullYear()
+export default {
+  name: 'FilterResultCard',
+  props: {
+    name: {
+      type: String,
+      default: '未命名'
+    },
+    type: {
+      type: String,
+      default: '無類別'
+    },
+    year: {
+      type: Number,
+      default: NaN
+    },
+    unitNumber: {
+      type: String,
+      default: NaN
+    },
+    totalPrice: {
+      type: Number,
+      default: NaN
+    },
+    unitPrice: {
+      type: Number,
+      default: NaN
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.FilterResultCard{
+  display: flex;
+  flex-direction: column;
+  margin: 5px;
+  padding: 10px;
+  border: 1px solid rgba(128, 128, 128, 0.377);
+  border-radius: 5px;
+  background: #ffffffaf;
+  box-shadow: 1px 1px 5px 0px #0000005e;
+  transition: 0.3s all;
+
+  &:hover{
+    background: #ffffffd8;
+    box-shadow: 2px 2px 5px 0px #0000005e;
+  }
+  & > * {
+    margin: 5px 0px;
+  }
+  .title{
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .subtitle{
+    display: flex;
+    .subitem{
+      margin-right: 5px;
+      margin-left: 5px;
+    }
+  }
+  .body{
+    display: flex;
+    .bodyitem{
+      flex: 1;
+      color: rgb(179, 48, 48);
+      font-size: 25px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
